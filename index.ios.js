c3426f61d3d0df1ee3be75f2f603eeb64f97f1a1
Jessica Kwok <jk3405@nyu.edu>
2016-09-06 23:11:02 -0700

    Add static initial state
    to get an idea of wtf im doin

--- index.ios.js
+++ index.ios.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react'; // why do i bracket this word "Component"
-import { AppRegistry, View, Text } from 'react-native'; // are these modules. if so, why did i import React and not like, { React } ?!? WHAT IS THIS
+import { AppRegistry, View, Text, Image, MapView } from 'react-native'; // are these modules. if so, why did i import React and not like, { React } ?!? WHAT IS THIS
 
 class WTFIsHappening extends Component {
   constructor(props) {
     super(props); // what this mean "super" oh duh obviously Component
     this.state = {
       item: {
+        imgUrl: "https://scontent-sjc2-1.cdninstagram.com/t51.2885-15/e35/14240729_630740280433346_989017859_n.jpg?ig_cache_key=MTMzMzAyMTU3NDI1NjY2MzQwOQ%3D%3D.2",
         name: "item",
         amount: 0,
         price: "$0"
+      },
+      jessicaCoors: {
+        lat: 37.782736,
+        lng: -122.485970
       }
     };
   }
   render() {
     return (
       <View>
-        <Text>Item Name: {this.state.item.name}</Text>
-        <Text>Number Remaining: {this.state.item.amount}</Text>
-        <Text>Price: {this.state.item.price}</Text>
+        <JessicaLocation coors={this.state.jessicaCoors}/>
+        <ItemView
+          item={this.state.item}
+        />
+      </View>
+    )
+  }
+}
+
+class ItemView extends Component {
+  render() {
+    return (
+      <View style={{margin: 40, marginTop: 0}}>
+        <Image source={{uri: this.props.item.imgUrl}} style={{height: 100}}/>
+        <Text>Item Name: {this.props.item.name}</Text>
+        <Text>Number Remaining: {this.props.item.amount}</Text>
+        <Text>Price: {this.props.item.price}</Text>
       </View>
     );
+  }
+}
+
+class JessicaLocation extends Component {
+  // the plan for this: center on SF, show my location from state
+  render() {
+    return (
+      <MapView
+        style={{height:200, margin: 40}}
+        region={{
+          latitude: this.props.coors.lat,
+          longitude: this.props.coors.lng,
+          latitudeDelta: .125,
+          longitudeDelta: .125
+        }}
+      />
+    )
   }
 }
 
