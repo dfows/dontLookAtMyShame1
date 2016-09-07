199c0b584a04bfe0137ff57d16acf2bf7b6926d6
Jessica Kwok <jk3405@nyu.edu>
2016-09-06 19:06:26 -0700

    Relearn what Components are
    damn son. how quickly knowledge of basic things can leave ur mind

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,23 @@
-// wat
+import React, { Component } from 'react'; // why do i bracket this word "Component"
+import { AppRegistry, View, Text } from 'react-native'; // are these modules. if so, why did i import React and not like, { React } ?!? WHAT IS THIS
+
+class WTFIsHappening extends Component {
+  render() {
+    return (
+      <View>
+        <AnotherComponent />
+        <AnotherComponent />
+      </View>
+    );
+  }
+}
+
+class AnotherComponent extends Component {
+  render() {
+    return (
+      <Text>I am another component.</Text>
+    )
+  }
+}
+
+AppRegistry.registerComponent('WTFIsHappening', () => WTFIsHappening);
