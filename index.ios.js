92d82a15b7d1df6bfc341297633b21314bc46f58
Jessica Kwok <jk3405@nyu.edu>
2016-09-07 13:33:58 -0700

    Put firebase
    da fk am i doing

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'; // why do i bracket this word "Component"
 import { AppRegistry, View, Text, Image, MapView } from 'react-native'; // are these modules. if so, why did i import React and not like, { React } ?!? WHAT IS THIS
+import * as firebase from 'firebase'; // yeah like how come i don't just import * from 'react'
 
 class WTFIsHappening extends Component {
   constructor(props) {
     super(props); // what this mean "super" oh duh obviously Component
+    const firebaseApp = firebase.initializeApp({ databaseURL: "https://mobile-bakery-10282.firebaseio.com" });
+    this.ordersRef = firebaseApp.database().ref("orders");
+
     this.state = {
       item: {
         imgUrl: "https://scontent-sjc2-1.cdninstagram.com/t51.2885-15/e35/14240729_630740280433346_989017859_n.jpg?ig_cache_key=MTMzMzAyMTU3NDI1NjY2MzQwOQ%3D%3D.2",
@@ -21,6 +25,22 @@ class WTFIsHappening extends Component {
       }
     };
   }
+
+  componentDidMount() {
+    // this confusing af
+    this.ordersRef.limitToFirst(1).once('child_added', (snap) => {
+      var order = snap.val();
+      this.setState({
+        item: {
+          imgUrl: order.itemImg || "none",
+          name: order.itemName,
+          amount: order.itemAmt,
+          price: order.itemPrice
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <View>
